fbf76abb7c817edee720f445151c6d60e5962a26
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-14 22:12:14 -0400

    commit tree 572566710c91fa9dd3771522f2c37e8a650afa69

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,25 +7,18 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  
+  extend Findable
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
 
   def add_song(song)
     @songs << song
@@ -36,7 +29,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  include Paramable
 end
